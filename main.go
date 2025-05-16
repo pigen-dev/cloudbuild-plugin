fa4fd77f34bcc3ea70b2fc0234c2b48177762836
aidocons <consultant-butlyai@aidodev.com>
2025-05-16 18:03:46 +0100

    fix plugin config

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	//"github.com/hashicorp/go-plugin"
 	//"os"
-	"fmt"
+	//"fmt"
 
-	"github.com/pigen-dev/cloudbuild-plugin/helpers"
+	//"github.com/pigen-dev/cloudbuild-plugin/helpers"
+	"github.com/hashicorp/go-plugin"
 	pkg "github.com/pigen-dev/cloudbuild-plugin/pkg"
 	shared "github.com/pigen-dev/shared"
 )
@@ -13,54 +13,54 @@ import (
 
 
 func main(){
-	pigenFileDummy := map[string]any{
-		"type": "cloudbuild",
-		"version": "v1.0.0",
-  	"repo_url": "",
-		"config": map[string]any{
-			"deployment": map[string]any{
-					"project_number": "775465758731",
-					"project_id":     "aidodev",
-					"project_region": "europe-west1",
-			},
-			"options": map[string]any{
-				"logging": "CLOUD_LOGGING_ONLY",
-			},
-			"github_url":    "https://github.com/aidocons/java-complete-pipeline.git",
-			"target_branch": "^develop$",
-		},
-		"steps": []map[string]any{
-			{
-				"step":         "mvn-compile",
-				"placeholders": map[string]any{
-					"flags": "",
-				}, // empty placeholders as dummy
-			},
-			{
-				"step":         "Docker-build-push",
-				"placeholders": map[string]any{
-					"image": "testing/add:latest",
-				}, // empty placeholders as dummy
-			},
-		},
-	}
-	pigenFile := shared.PigenStepsFile{}
-	err := helpers.YamlConfigParser(pigenFileDummy, &pigenFile)
-	if err != nil {
-		panic(err)
-	}
-	cloudbuildPlugin := &pkg.Cloudbuild{}
-	err = cloudbuildPlugin.CreateTrigger(pigenFile)
-	if err != nil {
-		fmt.Println("Error creating trigger:", err)
-		return
-	}
-	//os.WriteFile("cloudbuild.yaml", cicdFile.FileScript, 0644)
+	// pigenFileDummy := map[string]any{
+	// 	"type": "cloudbuild",
+	// 	"version": "v1.0.0",
+  // 	"repo_url": "",
+	// 	"config": map[string]any{
+	// 		"deployment": map[string]any{
+	// 				"project_number": "775465758731",
+	// 				"project_id":     "aidodev",
+	// 				"project_region": "europe-west1",
+	// 		},
+	// 		"options": map[string]any{
+	// 			"logging": "CLOUD_LOGGING_ONLY",
+	// 		},
+	// 		"github_url":    "https://github.com/aidocons/java-complete-pipeline.git",
+	// 		"target_branch": "^develop$",
+	// 	},
+	// 	"steps": []map[string]any{
+	// 		{
+	// 			"step":         "mvn-compile",
+	// 			"placeholders": map[string]any{
+	// 				"flags": "",
+	// 			}, // empty placeholders as dummy
+	// 		},
+	// 		{
+	// 			"step":         "Docker-build-push",
+	// 			"placeholders": map[string]any{
+	// 				"image": "testing/add:latest",
+	// 			}, // empty placeholders as dummy
+	// 		},
+	// 	},
+	// }
+	// pigenFile := shared.PigenStepsFile{}
+	// err := helpers.YamlConfigParser(pigenFileDummy, &pigenFile)
+	// if err != nil {
+	// 	panic(err)
+	// }
 	// cloudbuildPlugin := &pkg.Cloudbuild{}
-	// pluginMap := map[string]plugin.Plugin{"cicdPlugin": &shared.CicdPlugin{Impl: cloudbuildPlugin}}
+	// err = cloudbuildPlugin.CreateTrigger(pigenFile)
+	// if err != nil {
+	// 	fmt.Println("Error creating trigger:", err)
+	// 	return
+	// }
+	//os.WriteFile("cloudbuild.yaml", cicdFile.FileScript, 0644)
+	cloudbuildPlugin := &pkg.Cloudbuild{}
+	pluginMap := map[string]plugin.Plugin{"cicdPlugin": &shared.CicdPlugin{Impl: cloudbuildPlugin}}
 
-	// plugin.Serve(&plugin.ServeConfig{
-	// 	HandshakeConfig: shared.Handshake,
-	// 	Plugins:         pluginMap,
-	// })
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: shared.Handshake,
+		Plugins:         pluginMap,
+	})
 }
